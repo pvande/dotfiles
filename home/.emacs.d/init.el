4f9f4afc60f5b2b5f17de1d7f020168aeba6e256
Pieter van de Bruggen <pvande@gmail.com>
2011-01-14 12:39:04 -0800

    Adding kpm-list.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -2,5 +2,6 @@
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 (add-to-list 'load-path dotfiles-dir)
 
-;; Load ELPA.
-(require 'package)
+;; Load a few libraries.
+(require 'package)  ;; ELPA
+(require 'kpm-list) ;; Buffer grouping
