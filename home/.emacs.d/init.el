b129580946933be8391fd94cf2fe1ad9c96e2dce
Pieter van de Bruggen <pvande@gmail.com>
2011-01-15 09:29:55 -0800

    Setting up a "proper" cursor.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,3 +1,7 @@
+;; Blinking vertical bars are all the cursory rage...
+(setq cursor-type 'bar)
+(blink-cursor-mode t)
+
 ;; Add .emacs.d to the load path.
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 (add-to-list 'load-path dotfiles-dir)
