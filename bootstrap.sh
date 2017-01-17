8bd8e19b79fadc811771415398832e7b8149fe5e
Pieter van de Bruggen <pvande@gmail.com>
2017-01-17 13:40:53 -0800

    Adding Homebrew and Git to the bootstrap sequence.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+HOMEBREW_HOME="/usr/local/Homebrew"
+HOMEBREW_CORE_HOME="${HOMEBREW_HOME}/Library/Taps/homebrew/homebrew-core"
+HOMEBREW_TARBALL="https://github.com/Homebrew/brew/tarball/master"
+HOMEBREW_CORE_TARBALL="https://github.com/Homebrew/homebrew-core/tarball/master"
+PROJECTS_HOME="~/Projects"
+DOTFILES_REPO="https://github.com/pvande/dotfiles"
+
 abort() {
-  echo $'\e[31m!!!\e[m' "$@" $'\e[m'
+  echo '‼️ ' "$@"
   exit 1
 }
 
 announce() {
-  echo $'\e[34m***\e[m' "$@" $'\e[m'
+  echo '📢 ' "$@"
 }
 
 warn() {
-  echo $'\e[31m###\e[m' "$@" $'\e[m'
+  echo '⛔️ ' "$@"
 }
 
 info() {
-  echo $'\e[36m...\e[m' "$@" $'\e[m'
+  echo '💡 ' "$@"
+}
+
+good() {
+  echo '✅ ' "$@"
 }
 
 announce "Bootstrapping $(hostname -s)"
@@ -47,28 +58,59 @@ if $sudo_permissions || $sudo_xcode_tools; then
   warn "Releasing super-user privileges."
   sudo -k
 fi
+good "Permissions on /usr/local are correct..."
+good "Xcode Command Line Tools are present..."
 
+LOGFILE="${TMPDIR}/homebrew.log"
 if [ ! -x /usr/local/bin/brew ]; then
   info "Installing Homebrew..."
   mkdir -p /usr/local/{Cellar,Homebrew,Frameworks,bin,etc,include,lib,opt,sbin,share{,/zsh{,/site-functions}},var}
   mkdir -p ~/Library/Caches/Homebrew
   {
-    cd /usr/local/Homebrew &&
-    echo "Downloading and unpacking Homebrew..." > $TMPDIR/homebrew-install.log &&
-    /bin/bash -o pipefail -c '/usr/bin/curl -fsSL https://github.com/Homebrew/brew/tarball/master | /usr/bin/tar xz -m --strip 1' &&
-    ln -sf /usr/local/Homebrew/bin/brew /usr/local/bin/brew &&
-    mkdir -p /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core &&
-    cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core &&
-    echo "Downloading and unpacking Homebrew Core..." >> $TMPDIR/homebrew-install.log &&
-    /bin/bash -o pipefail -c '/usr/bin/curl -fsSL https://github.com/Homebrew/homebrew-core/tarball/master | /usr/bin/tar xz -m --strip 1' &&
-    echo 'Running `brew update --force`...' >> $TMPDIR/homebrew-install.log &&
-    brew update --force 2>&1 | cat >> $TMPDIR/homebrew-install.log
-  } || abort "Could not install Homebrew!  See $TMPDIR/homebrew-install.log for details."
+    echo "Downloading and unpacking Homebrew..." > $LOGFILE &&
+    cd $HOMEBREW_HOME &&
+    /bin/bash -o pipefail -c "/usr/bin/curl -fsSL $HOMEBREW_TARBALL | /usr/bin/tar xz -m --strip 1" &&
+    ln -sf ${HOMEBREW_HOME}/bin/brew /usr/local/bin/brew &&
+
+    echo "Downloading and unpacking Homebrew Core..." >> $LOGFILE &&
+    mkdir -p $HOMEBREW_CORE_HOME &&
+    cd $HOMEBREW_CORE_HOME &&
+    /bin/bash -o pipefail -c "/usr/bin/curl -fsSL $HOMEBREW_CORE_TARBALL | /usr/bin/tar xz -m --strip 1" &&
+
+    echo 'Running `brew update --force`...' >> $LOGFILE &&
+    brew update --force 2>&1 | cat >> $LOGFILE
+  } || abort "Could not install Homebrew!  See $LOGFILE for details."
 else
-  info "Updating Homebrew..."
   {
-    /usr/local/bin/brew update 2>&1 | cat >> $TMPDIR/homebrew-update.log
-  } || abort "Could not update Homebrew!  See $TMPDIR/homebrew-update.log for details."
+    /usr/local/bin/brew update 2>&1 | cat >> $LOGFILE
+  } || abort "Could not update Homebrew!  See $LOGFILE for details."
 fi
+good "Homebrew is up-to-date..."
+
+PATH="/usr/local/bin:$PATH"
+
+LOGFILE="${TMPDIR}/git.log"
+if !(/usr/local/bin/brew ls git &>/dev/null); then
+  info "Installing Git from Homebrew..."
+  {
+    /usr/local/bin/brew install git &>$LOGFILE
+  } || abort "Could not install Git!  See $LOGFILE for details."
+fi
+good "Git is present..."
+
+LOGFILE="${TMPDIR}/dotfiles.log"
+if [ ! -d $PROJECTS_HOME/dotfiles ]; then
+  info "Cloning $DOTFILES_REPO to ~/Projects/dotfiles..."
+  {
+    git clone $DOTFILES_REPO $PROJECTS_HOME/dotfiles &>$LOGFILE
+  } || abort "Could not clone $DOTFILES_REPO!  See $LOGFILE for details."
+elif !(cd ~/Projects/dotfiles && git fetch -q && git status | grep -q 'up-to-date'); then
+  info "Updating ~/Projects/dotfiles..."
+  {
+    cd ~/Projects/dotfiles &&
+    git merge origin/master &>$LOGFILE
+  } || abort "Could not update $DOTFILES_REPO!  See $LOGFILE for details."
+fi
+good "~/Projects/dotfiles is up-to-date..."
 
 announce "All set!"
