f3ef221f24e60d279715a05e97aa73c211d29535
Pieter van de Bruggen <pvande@gmail.com>
2017-01-17 23:28:36 -0800

    Adding zsh installation to the boostrapping config.

---
+++ config/zsh.sh
@@ -0,0 +1,16 @@
+if !(installed zsh); then
+  info "Installing zsh from Homebrew..."
+  {
+    $BINDIR/brew install zsh
+  } &>$LOGFILE || abort "Could not install zsh!"
+fi
+good "zsh is present..."
+
+export postinstall_zsh=""
+current_shell=$(dscl . -read /Users/$USER UserShell | awk '{ print $2 }')
+if [ $current_shell == "/usr/local/bin/zsh" ]; then
+  postinstall_zsh+="-"
+else
+  postinstall_zsh+="To set zsh as your default login shell,\n"
+  postinstall_zsh+="run \033[1;30mchsh -s /usr/local/bin/zsh\033[0m\n"
+fi
