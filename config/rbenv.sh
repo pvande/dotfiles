048b1b07a3ca067d6c876ad2135afd673b1e07a2
Pieter van de Bruggen <pvande@gmail.com>
2017-01-18 20:54:03 -0800

    Adding rbenv.

---
+++ config/rbenv.sh
@@ -0,0 +1,13 @@
+if !(installed rbenv); then
+  info "Installing rbenv from Homebrew..."
+  {
+    $BINDIR/brew install rbenv &&
+    $BINDIR/brew install rbenv-gemset &&
+    $BINDIR/brew install ruby-build
+  } &>$LOGFILE || abort "Could not install rbenv!"
+fi
+good "rbenv is present..."
+
+export postinstall_autoconf="-"
+export postinstall_openssl="-"
+export postinstall_rbenv="-"
