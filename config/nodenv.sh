579e270d6d21b4d58533320452a5f4b16282c369
Pieter van de Bruggen <pvande@gmail.com>
2017-01-20 12:25:28 -0800

    Adding yarn suppport.

--- config/nodenv.sh
+++
@@ -1,9 +0,0 @@
-if !(installed nodenv); then
-  info "Installing nodenv from Homebrew..."
-  {
-    $BINDIR/brew install nodenv
-  } &>$LOGFILE || abort "Could not install nodenv!"
-fi
-good "nodenv is present..."
-
-export postinstall_nodenv="-"
