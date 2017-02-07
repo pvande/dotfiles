8f2e3948ef2650e99c599bf08b0c4f38b15a183a
Pieter van de Bruggen <pvande@gmail.com>
2017-02-07 12:24:34 -0800

    Adding local node_modules to the PATH.

--- profile.d/__init__.sh
+++ profile.d/__init__.sh
@@ -1,2 +1,5 @@
+# Ensure that ./node_modules/.bin shows up early in $PATH
+export PATH=./node_modules/.bin:$PATH
+
 # Ensure that /usr/local/bin shows up early in $PATH
 export PATH=/usr/local/bin:$PATH
