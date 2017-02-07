9b919802201a6d2084e01f86963847d91a2e4809
Pieter van de Bruggen <pvande@gmail.com>
2017-02-07 10:35:35 -0800

    Adding additional applications.

--- config/applications.sh
+++ config/applications.sh
@@ -85,6 +85,8 @@ mappings="
   'Github Desktop': 'https://central.github.com/mac/latest'
   'Spectacle': 'https://s3.amazonaws.com/spectacle/downloads/Spectacle+1.2.zip'
   'WMail': 'https://github.com/Thomas101/wmail/releases/download/v2.0.0/WMail_2_0_0_osx.dmg'
+  'TogglDesktop': 'https://github.com/toggl/toggldesktop/releases/download/v7.4.7/TogglDesktop-7_4_7.dmg'
+  'Slack': 'https://slack.com/ssb/download-osx'
 "
 
 export -f info warn abort good
