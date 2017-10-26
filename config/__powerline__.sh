e83366a11927cf0179f59a696c44ab0d4eef5f15
Pieter van de Bruggen <pvande@gmail.com>
2017-10-25 20:27:20 -0700

    Powerline font of choice moved.

--- config/__powerline__.sh
+++ config/__powerline__.sh
@@ -1,21 +1,17 @@
 fonts="$HOME/Library/Fonts"
 
-if !(ls $fonts/Meslo.otf &>/dev/null); then
+if !(ls $fonts/Meslo.ttf &>/dev/null); then
   info "Installing Meslo for Powerline..."
   repo="https://github.com/powerline/fonts/raw/master"
 
   ### Hints ###
   # {L,M,S} - Line spacing?
   #   A line spacing of 0.9 works best with M...
-  # {DZ,} - Dotted Zero vs Slashed Zero
 
-  # font="Meslo LG L DZ Regular for Powerline.otf"
-  # font="Meslo LG L Regular for Powerline.otf"
-  # font="Meslo LG M DZ Regular for Powerline.otf"
-  font="Meslo LG M Regular for Powerline.otf"
-  # font="Meslo LG S DZ Regular for Powerline.otf"
-  # font="Meslo LG S Regular for Powerline.otf"
+  # font="Meslo LG L Regular for Powerline.ttf"
+  font="Meslo LG M Regular for Powerline.ttf"
+  # font="Meslo LG S Regular for Powerline.ttf"
 
-  curl -fsSL "$repo/Meslo/${font// /%20}" > "${fonts}/Meslo.otf"
+  curl -fsSL "$repo/Meslo%20Slashed/${font// /%20}" > "${fonts}/Meslo.ttf"
 fi
 good "Powerline fonts installed..."
