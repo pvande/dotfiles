8627bbc477f3baba6098e09c12ea8fad47cd2ad2
Pieter van de Bruggen <pvande@gmail.com>
2017-10-25 20:27:51 -0700

    No longer actively using either Clojure or Go.

--- config/clojure.sh
+++ config/clojure.sh
@@ -1,9 +1,9 @@
-if !(installed leiningen); then
-  info "Installing leiningen from Homebrew..."
-  {
-    $BINDIR/brew install leiningen
-  } &>$LOGFILE || abort "Could not install leiningen!"
-fi
-good "leiningen is present..."
-
-export postinstall_leiningen="-"
+# if !(installed leiningen); then
+#   info "Installing leiningen from Homebrew..."
+#   {
+#     $BINDIR/brew install leiningen
+#   } &>$LOGFILE || abort "Could not install leiningen!"
+# fi
+# good "leiningen is present..."
+#
+# export postinstall_leiningen="-"
