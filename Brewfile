04a684c7079f7773d4dc705eb28bdc912af90bc9
Pieter van de Bruggen <pvande@gmail.com>
2019-10-28 21:54:38 -0700

    Various cleanups

--- Brewfile
+++ Brewfile
@@ -2,6 +2,7 @@ tap "homebrew/bundle"
 tap "homebrew/cask"
 tap "homebrew/core"
 
+brew "git"
 brew "mas"
 
 cask "atom"
--- config/__homebrew__.sh
+++ config/__homebrew__.sh
@@ -1,2 +1,6 @@
 # Install the applications listed in the Brewfile.
 brew bundle
+
+export postinstall_git="-"
+export postinstall_mas="-"
+export postinstall_gettext="-"
