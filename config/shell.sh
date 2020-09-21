89b11bdcb46aa2fc680c1bc5550e790d17be8a68
Pieter van de Bruggen <pvande@gmail.com>
2020-09-21 14:30:00 -0700

    Misc. cleanup

--- config/shell.sh
+++ config/shell.sh
@@ -1,22 +1,22 @@
-if [ ! -d ~/.profile.d ]; then
+if [ ! -L ~/.profile.d ]; then
   info "Configuring shells..."
   {
     ln -s $DOTFILES_HOME/shell-rc.d $HOME/.profile.d
   } &>$LOGFILE || abort "Error adding shell config!"
 fi
 
-if [ ! -d ~/.bash.profile.d ]; then
+if [ ! -L ~/.bash.profile.d ]; then
   info "Configuring bash..."
   {
     ln -s $DOTFILES_HOME/bash.profile.d $HOME/.bash.profile.d
   } &>$LOGFILE || abort "Error adding bash config!"
 fi
 
-if [ ! -d ~/.zsh.profile.d ] || [ ! -r ~/.zprofile ]; then
+if [ ! -L ~/.zsh.profile.d ] || [ ! -L ~/.zshrc ]; then
   info "Configuring zsh..."
   {
-    ln -s $DOTFILES_HOME/shell-rc.sh $HOME/.zshrc &&
-    ln -s $DOTFILES_HOME/zsh.profile.d $HOME/.zsh.profile.d
+    ([ -L ~/.zsh.profile.d ] || ln -s $DOTFILES_HOME/zsh.profile.d $HOME/.zsh.profile.d) &&
+    ([ -L ~/.zshrc ] || ln -s $DOTFILES_HOME/shell-rc.sh $HOME/.zshrc)
   } &>$LOGFILE || abort "Error adding zsh config!"
 fi
 
