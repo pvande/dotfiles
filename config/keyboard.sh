7fb54e914cd7643f49a8295577cd2fe4708dc74d
Pieter van de Bruggen <pvande@gmail.com>
2020-09-21 16:52:50 -0700

    Updating keyboard configuration

--- config/keyboard.sh
+++ config/keyboard.sh
@@ -3,8 +3,8 @@ vendor_id_xpath="//key[contains(text(), 'VendorID')]/following-sibling::integer/
 product_id_xpath="//key[contains(text(), 'ProductID')]/following-sibling::integer/text()"
 xpath="concat($vendor_id_xpath, '-', $product_id_xpath, '-0')"
 
-# New setting: remap "Caps Lock" (30064771129) to "Control" (30064771296).
-remap='<dict><key>HIDKeyboardModifierMappingDst</key><integer>30064771303</integer><key>HIDKeyboardModifierMappingSrc</key><integer>30064771129</integer></dict>'
+# New setting: remap "Caps Lock" (30064771129) to "Control" (30064771300).
+remap='<dict><key>HIDKeyboardModifierMappingDst</key><integer>30064771300</integer><key>HIDKeyboardModifierMappingSrc</key><integer>30064771129</integer></dict>'
 
 # Write the setting.
 keyboard_id=$(ioreg -c AppleEmbeddedKeyboard -r -a | xmllint --xpath "$xpath" -)
